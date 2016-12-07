1db6d8f2c12f348169c74923d0b4c00462e9422c
sebvail <snowtoon@hotmail.com>
2016-12-07 10:38:34 -0500

    Tri fonctionnel
    localH gone

--- app/src/main/java/com/example/snowt/omnipucks/TestFragment.java
+++ app/src/main/java/com/example/snowt/omnipucks/TestFragment.java
@@ -124,10 +124,11 @@ public class TestFragment extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        /*
         if (id == R.id.action_settings) {
             return true;
         }
-
+        */
         return super.onOptionsItemSelected(item);
     }
 
